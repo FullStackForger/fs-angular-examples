683022cd7fcdef03753a79a1226f4c1e89d4c3d4
Mark Bevels <mark.bevels@gmail.com>
2015-06-17 17:29:28 +0100

    unit 07 deleted

--- units/unit_07/app/controllers.js
+++
@@ -1,8 +0,0 @@
-angular
-	.module('unit07Controllers', [])
-	.controller('MainController', ['$scope', function ($scope) {
-		$scope.title = 'Main Page';
-	}])
-	.controller('DetailsController', ['$scope', function($scope) {
-		$scope.title = 'Detail Page';
-	}]);
