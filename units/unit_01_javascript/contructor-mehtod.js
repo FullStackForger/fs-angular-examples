6fdd9b4f22a7680c2df9f96011e9eca393b44010
IndieForger <indieforger@gmail.com>
2016-03-08 06:06:40 +0000

    unit 01 updates

--- units/unit_01_javascript/contructor-mehtod.js
+++ units/unit_01_javascript/contructor-mehtod.js
@@ -1,7 +1,10 @@
-var obj1 = { a: 1 };
+var obj1 = { a: 1, func: function () { /** **/ } };
 var obj2 = new MyClass();
 
 // it is a CONSTRUCTOR METHOD!
 function MyClass() {
 	this.a = 1;
 }
+
+console.log(obj1.a);
+console.log(obj2.a);
--- units/unit_01_javascript/hoisting.js
+++ units/unit_01_javascript/hoisting.js
@@ -1,13 +1,14 @@
 // function executeMe() { .... } <- this is how JS hoists a mehtod
 
-// var t = "exposed on global scope";
+var t = "exposed on global scope";
 
 executeMe();
+
 console.log(t);
 
 function executeMe () {
-	//var t;  <- this is how JS hoists property
-	
+
+	var t;  //<- this is how JS hoists property
 	t = 0;
 	t++;
 	console.log(t);
